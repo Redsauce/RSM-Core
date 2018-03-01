8ccdf6b574fad3473032e8c1215b2f0b472a944d
Iván Fernández <ifernandez@redsauce.net>
2018-03-01 11:41:51 +0100

    Renamed definition for coherence

--- Server/htdocs/AppController/commands_RSM/updater/server/phpUpdate_From_v6.2.0.3.149_to_v6.3.0.3.150/update_post.sql
+++ Server/htdocs/AppController/commands_RSM/updater/server/phpUpdate_From_v6.2.0.3.149_to_v6.3.0.3.150/update_post.sql
@@ -1,3 +1,5 @@
 # Insert the application version with changes in the PHP layer
 INSERT INTO rs_dbchanges (RS_ID, RS_PREVIOUS_VERSION, RS_NEW_VERSION, RS_EXECUTION_DATE, RS_COMMENTS)
-VALUES (NULL, '6.2.0.3.149', '6.3.0.3.150', NOW(), 'No changes in the database');
+VALUES (NULL, '6.2.0.3.149', '6.3.0.3.150', NOW(), 'Renamed finantial document ready property definition to booked date');
+
+UPDATE rs_property_app_definitions SET RS_NAME = 'financial.documents.bookedDate', RS_DESCRIPTION = 'Indicates the date in which the document was booked' WHERE rs_property_app_definitions.RS_ID = 155; 
