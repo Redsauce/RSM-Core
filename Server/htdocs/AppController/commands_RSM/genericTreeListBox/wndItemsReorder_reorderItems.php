891c5e9f6e47f34963584875c78351954f3a867e
Iván Fernández <ifernandez@redsauce.net>
2018-07-12 21:25:32 +0200

    Reorder functionality moved to the classDataManager

--- Server/htdocs/AppController/commands_RSM/genericTreeListBox/wndItemsReorder_reorderItems.php
+++ Server/htdocs/AppController/commands_RSM/genericTreeListBox/wndItemsReorder_reorderItems.php
@@ -1,22 +1,9 @@
 <?php
-//***************************************************
-// Description:
-//***************************************************
+//****************************************************************
+// Description: Changes the internal order of the designated items
+//****************************************************************
 
 // Database connection startup
-include_once "../utilities/RSdatabase.php";
-include_once "../utilities/RSMitemsManagement.php";
-
-// Parameters validation
-isset($GLOBALS['RS_POST']['clientID'    ]) ? $clientID       =               $GLOBALS['RS_POST']['clientID'    ]  : dieWithError(400);
-isset($GLOBALS['RS_POST']['itemTypeID'  ]) ? $itemTypeID     =               $GLOBALS['RS_POST']['itemTypeID'  ]  : dieWithError(400);
-isset($GLOBALS['RS_POST']['parentID'    ]) ? $parentID       =               $GLOBALS['RS_POST']['parentID'    ]  : dieWithError(400);
-isset($GLOBALS['RS_POST']['propertyID'  ]) ? $propertyID     =               $GLOBALS['RS_POST']['propertyID'  ]  : dieWithError(400);
-isset($GLOBALS['RS_POST']['ids'         ]) ? $idList         = explode(",",  $GLOBALS['RS_POST']['ids'         ]) : dieWithError(400);
-isset($GLOBALS['RS_POST']['orders'      ]) ? $orderList      = explode(",",  $GLOBALS['RS_POST']['orders'      ]) : dieWithError(400);
-
-
-// Execute and write XML Response back to the application
-RSReturnArrayResults(reorderItems($clientID, $itemTypeID, $propertyID, $parentID, $idList, $orderList));
+include_once "../shared/classDataManager_reorderItems.php";
 
 ?>
