f817ed7ab2d75cd75733e52a8f7c5820ab7c3f82
Alfonso Abascal <aabascal@redsauce.net>
2020-10-22 10:21:54 +0200

    Archivos de actualización para una nueva versión

---
+++ Server/htdocs/AppController/commands_RSM/updater/server/phpUpdate_From_v6.7.2.3.161_to_v6.8.1.3.162/update_post.sql
@@ -0,0 +1,3 @@
+# Insert the application version with changes in the PHP layer
+INSERT INTO rs_dbchanges (RS_ID, RS_PREVIOUS_VERSION, RS_NEW_VERSION, RS_EXECUTION_DATE, RS_COMMENTS)
+VALUES (NULL, '6.7.2.3.161', '6.8.1.3.162', NOW(), 'No database changes');
