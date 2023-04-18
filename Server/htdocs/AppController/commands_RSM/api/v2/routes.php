6e64be89eb329d1bbc15c040be8f4e5548b3b37e
Javiera Maltas <jmaltas.redsauce@gmail.com>
2023-04-18 12:30:41 +0200

    Auttrail route

--- Server/htdocs/AppController/commands_RSM/api/v2/routes.php
+++ Server/htdocs/AppController/commands_RSM/api/v2/routes.php
@@ -12,9 +12,11 @@ $RSallowUncompressed = true;
 //ITEMS
 get('/RSM-Core/items', '/items/getItems.php');
 get('/RSM-Core/itemTypes', '/items/getItemTypes.php');
+get('/RSM-Core/audittrail', '/auditTrail/getAuditTrail.php');
 post('/RSM-Core/items', '/items/createItems.php');
 patch('/RSM-Core/items', '/items/updateItems.php');
 delete('/RSM-Core/items', '/items/deleteItems.php');
+
 
 //NOT FOUND
 if ($RSallowDebug) returnJsonMessage(404, "Endpoint not found");
