6f3ff67b87e2f93e1c2381d1cc6b7a428588d5cb
Alfonso Abascal <aabascal@redsauce.net>
2023-04-25 19:31:05 +0200

    It is now possible to obtain the staff element associated with a badge.

--- Server/htdocs/AppController/commands_RSM/shared/classDataManager_getStaffID.php
+++ Server/htdocs/AppController/commands_RSM/shared/classDataManager_getStaffID.php
@@ -5,9 +5,17 @@ require_once "../utilities/RStools.php";
 
 isset($GLOBALS["RS_POST"]["clientID"]) ? $clientID = $GLOBALS["RS_POST"]["clientID"] : dieWithError(400);
 isset($GLOBALS["RS_POST"]["login"   ]) ? $login    = $GLOBALS["RS_POST"]["login"   ] : dieWithError(400);
-isset($GLOBALS["RS_POST"]["password"]) ? $password = $GLOBALS["RS_POST"]["password"] : dieWithError(400);
+isset($GLOBALS["RS_POST"]["password"]) ? $password = $GLOBALS["RS_POST"]['password'] : $password = "";
 
-$theQuery = "SELECT RS_ITEM_ID as 'ID' FROM `rs_users` WHERE RS_LOGIN = '" . $login . "' AND RS_PASSWORD = '" . $password . "' AND RS_CLIENT_ID = '" . $clientID . "'";
+if ($password == "") {
+    // Verification with a badge
+    $theQuery = "SELECT RS_ITEM_ID as 'ID' FROM `rs_users` WHERE RS_BADGE = '" . $login . "' AND RS_CLIENT_ID = '" . $clientID . "'";
+
+} else {
+    // Verification with a username and password.
+    $theQuery = "SELECT RS_ITEM_ID as 'ID' FROM `rs_users` WHERE RS_LOGIN = '" . $login . "' AND RS_PASSWORD = '" . $password . "' AND RS_CLIENT_ID = '" . $clientID . "'";  
+
+}
 
 // Write back the XML Response to the application without compression
 RSReturnQueryResults(RSQuery($theQuery));
