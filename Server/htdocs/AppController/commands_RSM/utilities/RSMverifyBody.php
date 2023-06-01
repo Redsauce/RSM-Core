fb287724c813983ba57c5ca32cdf98a554769814
Gerard Planas <gplanas@redsauce.net>
2023-06-01 13:45:49 +0200

    added logs to verifyBody

--- Server/htdocs/AppController/commands_RSM/utilities/RSMverifyBody.php
+++ Server/htdocs/AppController/commands_RSM/utilities/RSMverifyBody.php
@@ -4,23 +4,27 @@
 // Functions to verify and ensure the accuracy of the request body (api) and its contents
 //*******************************************************************************************
 
-function checkIsJsonObject($item) {
+function checkIsJsonObject($item)
+{
   global $RSallowDebug;
   if (!is_object($item)) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "Invalid JSON Object '{}'");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
 }
 
-function checkIsArray($item) {
+function checkIsArray($item)
+{
   global $RSallowDebug;
   if (!is_null($item) and !is_array($item)) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "Invalid Array '[]'");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
@@ -30,34 +34,40 @@ function checkIsArray($item) {
 // Functions to verify and ensure that the request body (api) includes the mandatory items
 //*****************************************************************************************
 
-function checkBodyContains($body, $item) {
+function checkBodyContains($body, $item)
+{
   global $RSallowDebug;
   if (!isset($body->$item)) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "Request body must contain '{$item}'");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
 }
 
-function checkParamsContains($params, $item) {
+function checkParamsContains($params, $item)
+{
   global $RSallowDebug;
   if (!isset($params[$item])) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "Request must include query param '{$item}'");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
 }
 
-function checkBodyContainsAtLeastOne($body, $item1, $item2) {
+function checkBodyContainsAtLeastOne($body, $item1, $item2)
+{
   global $RSallowDebug;
   if (!(isset($body->$item1) or isset($body->$item2))) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "Request body must contain at least field '{$item1}' or field '{$item2}'");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
@@ -67,23 +77,27 @@ function checkBodyContainsAtLeastOne($body, $item1, $item2) {
 // Functions to verify and ensure that the request body (api) items are the correct type.
 //***************************************************************************************
 
-function checkIsInteger($item) {
+function checkIsInteger($item)
+{
   global $RSallowDebug;
   if (isset($item) && !is_int($item)) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "'{$item}' must be an integer");
     } else {
-    returnJsonMessage(400, "");
+      RSError("getRequestBody: Invalid JSON body");
+      returnJsonMessage(400, "");
     }
   }
 }
 
-function checkADJParamIsValid($body) {
+function checkADJParamIsValid($body)
+{
   global $RSallowDebug;
   if (isset($body["adj"]) && !preg_match('/^[sfwhdc]$/', $body["adj"])) {
     if ($RSallowDebug) {
       returnJsonMessage(400, "adj must be one of those characters: s, f, w, h, d, c");
     } else {
+      RSError("getRequestBody: Invalid JSON body");
       returnJsonMessage(400, "");
     }
   }
