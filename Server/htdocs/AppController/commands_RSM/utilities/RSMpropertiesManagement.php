879c44b3da9d05dd7eeb32fa0cb35cf13fe0c533
Gerard Planas <gplanas@redsauce.net>
2023-06-21 15:30:36 +0200

    RSMpropertiesManagement sonarqube changes

--- Server/htdocs/AppController/commands_RSM/utilities/RSMpropertiesManagement.php
+++ Server/htdocs/AppController/commands_RSM/utilities/RSMpropertiesManagement.php
@@ -1,20 +1,20 @@
 <?php
 $propertiesTables = array(
-	'text'       				  => 'rs_property_text',
-	'longtext'			   	      => 'rs_property_longtext',
-	'date'   		    		  => 'rs_property_dates',
-	'integer'    				  => 'rs_property_integers',
-	'float'      				  => 'rs_property_floats',
-	'datetime'   				  => 'rs_property_datetime',
-	'identifier' 				  => 'rs_property_identifiers',
-	'identifier2itemtype'         => 'rs_property_identifiers_to_itemtypes',
-	'identifier2property'         => 'rs_property_identifiers_to_properties',
-	'identifiers'				  => 'rs_property_multiIdentifiers',
-	'image'		 				  => 'rs_property_images',
-	'password'   				  => 'rs_property_passwords',
-	'variant'    				  => 'rs_property_variant',
-	'color'    					  => 'rs_property_colors',
-	'file'    					  => 'rs_property_files'
+    'text'                         => 'rs_property_text',
+    'longtext'                         => 'rs_property_longtext',
+    'date'                         => 'rs_property_dates',
+    'integer'                      => 'rs_property_integers',
+    'float'                        => 'rs_property_floats',
+    'datetime'                     => 'rs_property_datetime',
+    'identifier'                   => 'rs_property_identifiers',
+    'identifier2itemtype'         => 'rs_property_identifiers_to_itemtypes',
+    'identifier2property'         => 'rs_property_identifiers_to_properties',
+    'identifiers'                  => 'rs_property_multiIdentifiers',
+    'image'                           => 'rs_property_images',
+    'password'                     => 'rs_property_passwords',
+    'variant'                      => 'rs_property_variant',
+    'color'                          => 'rs_property_colors',
+    'file'                          => 'rs_property_files'
 );
 
 $propertiesDefaultValues = array(
@@ -54,33 +54,33 @@ $propertiesValidationExpressions = array(
 );
 
 $auditTrailPropertiesTables = array(
-	'text'     				    => 'rs_audit_trail_property_text',
-	'longtext' 				    => 'rs_audit_trail_property_longtext',
-	'date'     				    => 'rs_audit_trail_property_dates',
-	'integer'  				    => 'rs_audit_trail_property_integers',
-	'float'    				    => 'rs_audit_trail_property_floats',
-	'datetime' 				    => 'rs_audit_trail_property_datetime',
-	'identifier' 			    => 'rs_audit_trail_property_identifiers',
-	'identifier2itemtype'       => 'rs_audit_trail_property_identifiers_to_itemtypes',
-	'identifier2property'       => 'rs_audit_trail_property_identifiers_to_properties',
-	'identifiers'			    => 'rs_audit_trail_property_multiIdentifiers',
-	'image'					    => 'rs_audit_trail_property_images',
-	'password'  			    => 'rs_audit_trail_property_passwords',
-	'variant'   			    => 'rs_audit_trail_property_variant',
-	'color'   				    => 'rs_audit_trail_property_colors',
-	'file'   				    => 'rs_audit_trail_property_files'
+    'text'                         => 'rs_audit_trail_property_text',
+    'longtext'                     => 'rs_audit_trail_property_longtext',
+    'date'                         => 'rs_audit_trail_property_dates',
+    'integer'                      => 'rs_audit_trail_property_integers',
+    'float'                        => 'rs_audit_trail_property_floats',
+    'datetime'                     => 'rs_audit_trail_property_datetime',
+    'identifier'                 => 'rs_audit_trail_property_identifiers',
+    'identifier2itemtype'       => 'rs_audit_trail_property_identifiers_to_itemtypes',
+    'identifier2property'       => 'rs_audit_trail_property_identifiers_to_properties',
+    'identifiers'                => 'rs_audit_trail_property_multiIdentifiers',
+    'image'                        => 'rs_audit_trail_property_images',
+    'password'                  => 'rs_audit_trail_property_passwords',
+    'variant'                   => 'rs_audit_trail_property_variant',
+    'color'                       => 'rs_audit_trail_property_colors',
+    'file'                       => 'rs_audit_trail_property_files'
 );
 
 
-function enforcePropertyType($value, $clientID, $propertyID, $propertyType) {
-    global $propertiesTables, $propertiesDefaultValues, $propertiesValidationExpressions, $RSuserID, $RStoken;
+function enforcePropertyType($value, $clientID, $propertyID, $propertyType)
+{
+    global $propertiesDefaultValues, $propertiesValidationExpressions;
 
     if ($propertyType == "identifiers") {
 
         // Clean leading, trailing and duplicated commas
         $value = trim($value, ",");
         $value = preg_replace("/,+/", ",", $value);
-
     }
 
     //check value match the right format (if match partially, just take the first valid part of the value)
@@ -88,7 +88,6 @@ function enforcePropertyType($value, $clientID, $propertyID, $propertyType) {
 
         // match found, we take the first valid match
         $value = $extracted[0];
-
     } else {
 
         // no match found (wrong format), set to the default value for the property
@@ -99,17 +98,13 @@ function enforcePropertyType($value, $clientID, $propertyID, $propertyType) {
 
             // match found, we take the first valid match
             $value = $extracted[0];
-
         } else {
 
             // no match found in default value (wrong format), set to the system default value for the property type
             $value = $propertiesDefaultValues[$propertyType];
-
         }
-
     }
 
 
     return $value;
 }
-?>
