d0856734ab31196879696611cddec1dbe9be12b1
Javiera Maltas <jmaltas@redsauce.net>
2023-06-22 13:16:10 +0200

    RSsecurityCheck sonarqube changes

--- Server/htdocs/AppController/commands_RSM/utilities/RSsecurityCheck.php
+++ Server/htdocs/AppController/commands_RSM/utilities/RSsecurityCheck.php
@@ -3,19 +3,19 @@
 //RSsecurityCheck.php
 //***************************************************
 //Description:
-//	checks if the application version is compatible with
+//  checks if the application version is compatible with
 //  the database in use, and also checks if the user
 //  has privileges to work with the system
 //***************************************************
 //Version:
-//	v1.0: checks if the application is compatible with
+//  v1.0: checks if the application is compatible with
 //        the database and then validates passed login
 //        and password against DB
 //  v1.1: It uses the rs_users table for all the apps
 //***************************************************
 //Input: POST
-//	         RSuserID: user's login
-//	RSuserMD5Password: user's password encrypted in MD5
+//           RSuserID: user's login
+//  RSuserMD5Password: user's password encrypted in MD5
 //            RSbuild: Application build
 //          RSappName: Application Name
 //         RSlanguage: Application language
@@ -28,47 +28,57 @@
 require_once "RSvalidationFunctions.php";
 require_once "RSMtokensManagement.php";
 
+define('ACCESS_DENIED_MSG', 'ACCESS DENIED');
+
 $RSuserID =  0; // By default there is not a defined user
 
 if (isset($GLOBALS['RS_GET']['r'])) {
-    $parameters = explode("&", rtrim(mcrypt_decrypt("blowfish", $RSblowfishKey, pack("H*" , $GLOBALS['RS_GET']['r']), "ecb"), "\x05"));;
+    $parameters = explode("&", rtrim(mcrypt_decrypt("blowfish", $RSblowfishKey, pack("H*", $GLOBALS['RS_GET']['r']), "ecb"), "\x05"));
 
     foreach ($parameters as $parameter) {
         $parameter = explode("=", $parameter);
         $GLOBALS['RS_GET'][$parameter[0]] = $parameter[1];
     }
-
     unset($GLOBALS['RS_GET']['r']);
 }
 
 // If a clientID is given...
 if (isset($GLOBALS['RS_POST']['clientID'])) {
-	// and a token is given too...
-	$RSuserID = RSCheckUserAccess();
+    // and a token is given too...
+    $RSuserID = RSCheckUserAccess();
 
-	if (isset($GLOBALS['RS_POST']['RStoken'])) {
-		// validates if their associated clients match.
-		if ($GLOBALS['RS_POST']['clientID'] != RSClientFromToken($GLOBALS['RS_POST']['RStoken'])) RSReturnError("ACCESS DENIED", -3);
-
-	} else {
-		// We don't have a token so validate user permissions
-		if (RSCheckCompatibleDB(0) == 0) RSReturnError("INCOMPATIBLE VERSION", -4);
-		if ($RSuserID == 0             ) RSReturnError("ACCESS DENIED", -3);
-	}
+    if (isset($GLOBALS['RS_POST']['RStoken'])) {
+        // validates if their associated clients match.
+        if ($GLOBALS['RS_POST']['clientID'] != RSClientFromToken($GLOBALS['RS_POST']['RStoken'])) {
+            RSReturnError(ACCESS_DENIED_MSG, -3);
+        }
+    } else {
+        // We don't have a token so validate user permissions
+        if (RSCheckCompatibleDB(0) == 0) {
+            RSReturnError("INCOMPATIBLE VERSION", -4);
+        }
+        if ($RSuserID == 0) {
+            RSReturnError(ACCESS_DENIED_MSG, -3);
+        }
+    }
 
 } elseif (isset($GLOBALS['RS_POST']['RStoken'])) {
-	// If we don't have a clientID, validates if there is a valid token sent through POST
-	$GLOBALS['RS_POST']['clientID'] = RSClientFromToken($GLOBALS['RS_POST']['RStoken']);
-	if ($GLOBALS['RS_POST']['clientID'] <= 0) RSReturnError("ACCESS DENIED", -3);
+    // If we don't have a clientID, validates if there is a valid token sent through POST
+    $GLOBALS['RS_POST']['clientID'] = RSClientFromToken($GLOBALS['RS_POST']['RStoken']);
+    if ($GLOBALS['RS_POST']['clientID'] <= 0) {
+        RSReturnError(ACCESS_DENIED_MSG, -3);
+    }
 
 } elseif (isset($GLOBALS['RS_GET']['RStoken'])) {
     // If we don't have a clientID, validates if there is a valid token sent through GET
     $GLOBALS['RS_POST']['clientID'] = RSClientFromToken($GLOBALS['RS_GET']['RStoken']);
-    if ($GLOBALS['RS_POST']['clientID'] <= 0) RSReturnError("ACCESS DENIED", -3);
+    if ($GLOBALS['RS_POST']['clientID'] <= 0) {
+        RSReturnError(ACCESS_DENIED_MSG, -3);
+    }
 
 } else {
-	// By default we check if the database is compatible
-	if (RSCheckCompatibleDB(0) == 0) RSReturnError("INCOMPATIBLE VERSION", -4);
-
+    // By default we check if the database is compatible
+    if (RSCheckCompatibleDB(0) == 0) {
+        RSReturnError("INCOMPATIBLE VERSION", -4);
+    }
 }
-?>
