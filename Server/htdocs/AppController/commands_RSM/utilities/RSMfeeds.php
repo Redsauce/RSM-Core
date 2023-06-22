4f9ad5da99c2855bbc6056a85c8b9ecbd41eef29
Gerard Planas <gplanas@redsauce.net>
2023-06-22 09:43:52 +0200

    RSMfeeds sonarqube changes

--- Server/htdocs/AppController/commands_RSM/utilities/RSMfeeds.php
+++ Server/htdocs/AppController/commands_RSM/utilities/RSMfeeds.php
@@ -4,7 +4,8 @@ require_once "RSconfiguration.php";
 /*
  * Get Feed Info (newsType)
  */
-function getNewsType($token, $tema) {
+function getNewsType($token, $tema)
+{
     global $RSMapiURL;
     $ch = curl_init($RSMapiURL . "api_getItems.php");
     $propertiesId = "newsType.description,newsType.language,newsType.URL";
@@ -22,27 +23,27 @@ function getNewsType($token, $tema) {
     $xml = simplexml_load_string($xmlret);
 
     foreach ($xml->rows->row as $row) {
-        return $row -> column;
+        return $row->column;
     }
-
 }
 
-function getRSS($RSStitle, $token, $propertyIDs, $filterRules, $extFilterRules, $filterJoining) {
+function getRSS($rssTitle, $token, $propertyIDs, $filterRules, $extFilterRules, $filterJoining)
+{
     global $RSMapiURL;
 
-    $newsType = getNewsType($token, $RSStitle);
+    $newsType = getNewsType($token, $rssTitle);
 
     if ($newsType[0] != "NOK") {
-        $RSSdescription = $newsType[1];
-        $RSSlanguage    = $newsType[2];
-        $RSSlink        = $newsType[3];
+        $rssDescription = $newsType[1];
+        $rssLanguage    = $newsType[2];
+        $rssLink        = $newsType[3];
     } else {
-        $RSSdescription = "";
-        $RSSlanguage = "";
-        $RSSlink = "";
+        $rssDescription = "";
+        $rssLanguage = "";
+        $rssLink = "";
     }
 
-    // Get News	 Info
+    // Get News Info
     $ch = curl_init($RSMapiURL . "api_getItems.php");
     $data = "RStoken=" . $token . "&propertyIDs=" . $propertyIDs . "&filterRules=" . $filterRules . "&extFilterRules=" . $extFilterRules . "&filterJoining=" . $filterJoining;
 
@@ -61,68 +62,67 @@ function getRSS($RSStitle, $token, $propertyIDs, $filterRules, $extFilterRules, 
 
     // Create RSS Document
     $pDom = new DOMDocument();
-    $pRSS = $pDom -> createElement('rss');
-    $pRSS -> setAttribute('version', "2.0");
-    $pDom -> appendChild($pRSS);
-    $pChannel = $pDom -> createElement('channel');
-    $pRSS -> appendChild($pChannel);
+    $pRSS = $pDom->createElement('rss');
+    $pRSS->setAttribute('version', "2.0");
+    $pDom->appendChild($pRSS);
+    $pChannel = $pDom->createElement('channel');
+    $pRSS->appendChild($pChannel);
 
-    $pChannel -> appendChild($pDom -> createElement('title', $RSStitle));
-    $pChannel -> appendChild($pDom -> createElement('link', $RSSlink));
-    $pChannel -> appendChild($pDom -> createElement('description', $RSSdescription));
-    $pChannel -> appendChild($pDom -> createElement('language', $RSSlanguage));
+    $pChannel->appendChild($pDom->createElement('title', $rssTitle));
+    $pChannel->appendChild($pDom->createElement('link', $rssLink));
+    $pChannel->appendChild($pDom->createElement('description', $rssDescription));
+    $pChannel->appendChild($pDom->createElement('language', $rssLanguage));
 
     foreach ($xml->rows->row as $row) {
-        $pItem = $pDom -> createElement('item');
-        $pChannel -> appendChild($pItem);
+        $pItem = $pDom->createElement('item');
+        $pChannel->appendChild($pItem);
 
-        $newsId      = htmlspecialchars($row -> column[0]);
-        $titulo      = htmlspecialchars($row -> column[1]);
-        $descripcion = htmlspecialchars($row -> column[2]);
-        $imagenName  = htmlspecialchars($row -> column[3]);
-        $autor       = htmlspecialchars($row -> column[4]);
-        $fecha       = htmlspecialchars($row -> column[5]);
-        $link        = htmlspecialchars($row -> column[6]);
+        $newsId      = htmlspecialchars($row->column[0]);
+        $titulo      = htmlspecialchars($row->column[1]);
+        $descripcion = htmlspecialchars($row->column[2]);
+        $imagenName  = htmlspecialchars($row->column[3]);
+        $autor       = htmlspecialchars($row->column[4]);
+        $fecha       = htmlspecialchars($row->column[5]);
+        $link        = htmlspecialchars($row->column[6]);
 
         if ($titulo != "") {
-            $pTitle = $pDom -> createElement('title', $titulo);
-            $pItem -> appendChild($pTitle);
+            $pTitle = $pDom->createElement('title', $titulo);
+            $pItem->appendChild($pTitle);
         }
 
         if ($descripcion != "") {
-            $pDesc = $pDom -> createElement('description', $descripcion);
-            $pItem -> appendChild($pDesc);
+            $pDesc = $pDom->createElement('description', $descripcion);
+            $pItem->appendChild($pDesc);
         }
 
         if ($link != "") {
-            $pLink = $pDom -> createElement('link', $link);
-            $pItem -> appendChild($pLink);
+            $pLink = $pDom->createElement('link', $link);
+            $pItem->appendChild($pLink);
         }
 
         if ($autor != "") {
-            $pAuthor = $pDom -> createElement('author', $autor);
-            $pItem -> appendChild($pAuthor);
+            $pAuthor = $pDom->createElement('author', $autor);
+            $pItem->appendChild($pAuthor);
         }
 
         if ($fecha != "") {
-            $pDate = $pDom -> createElement('pubDate', $fecha);
-            $pItem -> appendChild($pDate);
+            $pDate = $pDom->createElement('pubDate', $fecha);
+            $pItem->appendChild($pDate);
         }
 
         if ($imagenName != "" && $imagenName != ":0") {
-            $pImg = $pDom -> createElement('image');
-            $pItem -> appendChild($pImg);
+            $pImg = $pDom->createElement('image');
+            $pItem->appendChild($pImg);
 
             $urlGetPicture = $RSMapiURL . "api_getPicture.php";
             $imageUrl = $urlGetPicture . "?itemID=" . $newsId . "&amp;propertyID=606&amp;RStoken=" . $token;
-            $pImgUrl = $pDom -> createElement('url', $imageUrl);
-            $pImg -> appendChild($pImgUrl);
+            $pImgUrl = $pDom->createElement('url', $imageUrl);
+            $pImg->appendChild($pImgUrl);
 
-            $pImgTitle = $pDom -> createElement('title', $imagenName);
-            $pImg -> appendChild($pImgTitle);
+            $pImgTitle = $pDom->createElement('title', $imagenName);
+            $pImg->appendChild($pImgTitle);
         }
     }
 
-    return $pDom -> saveXML();
+    return $pDom->saveXML();
 }
-?>
