35aa366cb5c58ee3dbde6dd30afe7c716bc033c1
Javiera Maltas <jmaltas@redsauce.net>
2023-06-22 13:16:38 +0200

    RSregisterError sonarqube changes

--- Server/htdocs/AppController/commands_RSM/utilities/RSregisterError.php
+++ Server/htdocs/AppController/commands_RSM/utilities/RSregisterError.php
@@ -10,23 +10,22 @@ $result = base64_decode($GLOBALS['RS_POST']['result']);
 $query = "INSERT INTO `rs_error_log` (`RS_DATE`,`RS_URL`,`RS_POST`,`RS_RESULT`) VALUES (NOW(),'".$mysqli->real_escape_string($url)."','".$mysqli->real_escape_string($post)."','".$mysqli->real_escape_string($result)."')";
 
 // Query the database
-if(RSquery($query)){
-	//send mail to admin
-	/*$mensaje = "Se ha registrado un nuevo error en RSM:\n\nURL:\n".$url."\n\nPOST DATA SENT:\n".$post."\n\nSERVER RESPONSE:\n".$result."";
-	$mensaje = wordwrap($mensaje, 70);
-	mail('webmaster@redsauce.net', 'Nuevo error en RSM', $mensaje);*/
+if (RSquery($query)) {
+    //send mail to admin
+    /*$mensaje = "Se ha registrado un nuevo error en RSM:\n\nURL:\n".$url."\n\nPOST DATA SENT:\n".$post."\n\nSERVER RESPONSE:\n".$result."";
+    $mensaje = wordwrap($mensaje, 70);
+    mail('webmaster@redsauce.net', 'Nuevo error en RSM', $mensaje);*/
 
-	$results['result'] = "OK";
-	$results['ID'] = $mysqli->insert_id;
-}else{
-	//send mail to admin
-	/*$mensaje = "Se ha notificado un nuevo error en RSM, este resultado NO HA PODIDO SER ALMACENADO EN LA BD:\n\nURL:\n".$url."\n\nPOST DATA SENT:\n".$post."\n\nSERVER RESPONSE:\n".$result."";
-	$mensaje = wordwrap($mensaje, 70);
-	mail('webmaster@redsauce.net', 'Nuevo error en RSM (NO ALMACENADO)', $mensaje);*/
+    $results['result'] = "OK";
+    $results['ID'] = $mysqli->insert_id;
+} else {
+    //send mail to admin
+    /*$mensaje = "Se ha notificado un nuevo error en RSM, este resultado NO HA PODIDO SER ALMACENADO EN LA BD:\n\nURL:\n".$url."\n\nPOST DATA SENT:\n".$post."\n\nSERVER RESPONSE:\n".$result."";
+    $mensaje = wordwrap($mensaje, 70);
+    mail('webmaster@redsauce.net', 'Nuevo error en RSM (NO ALMACENADO)', $mensaje);*/
 
-	$results['result'] = "NOK";
+    $results['result'] = "NOK";
 }
 
 // Write XML Response back to the application
 RSReturnArrayResults($results);
-?>
