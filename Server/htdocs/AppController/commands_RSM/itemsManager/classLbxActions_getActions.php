664cdb5af16570989d0d4044d5c3febbd7aef3a5
Javiera Maltas <jmaltas@redsauce.net>
2023-06-29 13:39:55 +0200

    Sonarqube changes in classLbxActions

--- Server/htdocs/AppController/commands_RSM/itemsManager/classLbxActions_getActions.php
+++ Server/htdocs/AppController/commands_RSM/itemsManager/classLbxActions_getActions.php
@@ -1,7 +1,7 @@
 <?php
 //***************************************************
 //Description:
-//	Returns all actions for the RSM application
+//  Returns all actions for the RSM application
 // ---> updated for the v.3.10
 //***************************************************
 
@@ -11,8 +11,9 @@ require_once "../utilities/RSdatabase.php";
 // Now we build the query
 $theQuery = "SELECT `rs_actions`.`RS_ID`, `rs_actions`.`RS_NAME`, `rs_actions`.`RS_DESCRIPTION` FROM `rs_actions`";
 
-if($GLOBALS['RS_POST']['applicationName']!=0)
-	$theQuery .= " WHERE `rs_actions`.`RS_APPLICATION_NAME` = '".$GLOBALS['RS_POST']['applicationName']."'";
+if ($GLOBALS['RS_POST']['applicationName']!=0) {
+    $theQuery .= " WHERE `rs_actions`.`RS_APPLICATION_NAME` = '".$GLOBALS['RS_POST']['applicationName']."'";
+}
 
 
 $theQuery .= " ORDER BY `rs_actions`.`RS_NAME` DESC";
@@ -22,4 +23,3 @@ $results = RSquery($theQuery);
 
 // And write XML Response back to the application
 RSReturnQueryResults($results);
-?>
