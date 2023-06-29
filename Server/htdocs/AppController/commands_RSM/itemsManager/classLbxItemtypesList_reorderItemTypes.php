d23f6fcd40acdb7275ddc39337300d184d61ecb0
Javiera Maltas <jmaltas@redsauce.net>
2023-06-29 12:43:43 +0200

    Sonarqube changes classLbxItemTypesList group

--- Server/htdocs/AppController/commands_RSM/itemsManager/classLbxItemtypesList_reorderItemTypes.php
+++ Server/htdocs/AppController/commands_RSM/itemsManager/classLbxItemtypesList_reorderItemTypes.php
@@ -5,26 +5,21 @@ require_once "../utilities/RSdatabase.php";
 // init arrays with comma separated posted values
 $idList = explode(',', $GLOBALS['RS_POST']['ids']);
 
-
 for ($i = 0; $i < count($idList); $i++) {
-	// reorder the element
-	$theQuery = 'UPDATE rs_item_types SET RS_ORDER = '.($i+1).' WHERE (RS_ITEMTYPE_ID = '.$idList[$i].' AND RS_CLIENT_ID = '. $GLOBALS['RS_POST']['clientID'].')';
+    // reorder the element
+    $theQuery = 'UPDATE rs_item_types SET RS_ORDER = '.($i+1).' WHERE (RS_ITEMTYPE_ID = '.$idList[$i].' AND RS_CLIENT_ID = '. $GLOBALS['RS_POST']['clientID'].')';
 
-	// execute query
-	if (!RSQuery($theQuery)) {
-		// return NOK
-		$results['result'] = "NOK";
+    // execute query
+    if (!RSQuery($theQuery)) {
+        // return NOK
+        $results['result'] = "NOK";
 
-		// Write XML Response back to the application
-		RSReturnArrayResults($results);
-		exit;
-	}
+        // Write XML Response back to the application
+        RSReturnArrayResults($results);
+        exit;
+    }
 }
-
-
-
 $results['result'] = "OK";
 
 // And write XML Response back to the application
 RSReturnArrayResults($results);
-?>
