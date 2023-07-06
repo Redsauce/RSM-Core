2c1e14559868f40ad884d132427ec27c5f9ec5df
Gerard Planas <gplanas@redsauce.net>
2023-07-06 11:41:11 +0200

    MainPanel sonarqube changes

--- Server/htdocs/AppController/commands_RSM/mainPanel/wndMainPanel_getSearchItemTypes.php
+++ Server/htdocs/AppController/commands_RSM/mainPanel/wndMainPanel_getSearchItemTypes.php
@@ -1,7 +1,7 @@
 <?php
 //***************************************************//
 //Description:
-//	This PHP receives a textFilter and returns an array with the names.
+//  This PHP receives a textFilter and returns an array with the names.
 //  and identifiers of the itemTypes where the textFilter appears,
 //  and the number of occurrences.
 //  Only the allowed item types for the user are returned.
@@ -11,19 +11,16 @@
 require_once '../utilities/RSdatabase.php';
 require_once '../utilities/RSMitemsManagement.php';
 
-isset($GLOBALS["RS_POST"]["clientID"  ]) ? $clientID   = $GLOBALS["RS_POST"]["clientID"  ] : dieWithError(400);
+isset($GLOBALS["RS_POST"]["clientID"]) ? $clientID   = $GLOBALS["RS_POST"]["clientID"] : dieWithError(400);
 isset($GLOBALS["RS_POST"]["textFilter"]) ? $textFilter = $GLOBALS["RS_POST"]["textFilter"] : dieWithError(400);
 
 //First of all, we need to check if the variable clientID does not have the value 0
-if ($clientID != 0 AND $RSuserID != 0) {
-  $filteredResults = getItemTypeIDs_usingFilter($RSuserID,$clientID,$textFilter);
+if ($clientID != 0 && $RSuserID != 0) {
+  $filteredResults = getItemTypeIDs_usingFilter($RSuserID, $clientID, $textFilter);
 
   // Write XML Response back to the application
   RSReturnArrayQueryResults($filteredResults, false);
-
 } else {
-    $results['result'] = "NOK";
-    RSReturnArrayResults($results);
-
+  $results['result'] = "NOK";
+  RSReturnArrayResults($results);
 }
-?>
