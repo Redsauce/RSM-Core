c366de5e4ede334bda13b87ea621f58a35849d93
Javiera Maltas <jmaltas@redsauce.net>
2023-07-06 12:01:39 +0200

    Sonarqube changes in gql

--- Server/htdocs/AppController/commands_RSM/gql/get.php
+++ Server/htdocs/AppController/commands_RSM/gql/get.php
@@ -37,7 +37,7 @@ $replace = array("&rsquo;" , "&quot;");
 
 // Get graphQL query
 $inputJSON = file_get_contents('php://input');
-$input = json_decode($inputJSON, TRUE); //convert JSON into array
+$input = json_decode($inputJSON, true); //convert JSON into array
 
 // Define that output will be compressed if possible
 ob_start('ob_gzhandler');
@@ -84,8 +84,8 @@ foreach ($countriesList as $country) {
       $products[]=array("Name"=>html_entity_decode($product['Product'], ENT_COMPAT, "UTF-8"),"Product Status"=>html_entity_decode($product['Status'], ENT_COMPAT, "UTF-8"));
    }
 
-   if($country['Region']!=$currentRegion){
-      if(count($countries)>0){
+   if ($country['Region']!=$currentRegion) {
+      if (!empty($countries)) {
          $regions[]=array("Name"=>html_entity_decode($currentRegion, ENT_COMPAT, "UTF-8"),"Country"=>$countries);
          $countries=array();
       }
@@ -94,7 +94,7 @@ foreach ($countriesList as $country) {
    $countries[]=array("Name"=>html_entity_decode($country['Country'], ENT_COMPAT, "UTF-8"),"Product"=>$products);
 }
 //store last country products
-if(count($countries)>0){
+if (!empty($countries)) {
    $regions[]=array("Name"=>html_entity_decode($currentRegion, ENT_COMPAT, "UTF-8"),"Country"=>$countries);
 }
 
@@ -127,11 +127,9 @@ $result = array("Region"=>$regions);
 // Return data converted to JSON (and compressed if possible)
 echo json_encode(array("data"=>$result));
 
-
-
-
 // Function to parse all elements inside an {} element of the query structure
-function parseLevel ($input, $linkingProperty = 0, $parentValue = 0) {
+function parseLevel($input, $linkingProperty = 0, $parentValue = 0)
+{
    $properties = array();
    $results = array();
 
@@ -139,7 +137,7 @@ function parseLevel ($input, $linkingProperty = 0, $parentValue = 0) {
    while ($input != "") {
       // Get position of next line delimiter
       // TODO elements can be splitted by spaces and commas also
-      $newLinePos = strpos ($input, "\n");
+      $newLinePos = strpos($input, "\n");
 
       // Get the line and remove from input
       if ($newLinePos === false) {
@@ -154,22 +152,22 @@ function parseLevel ($input, $linkingProperty = 0, $parentValue = 0) {
 
       if ($cleanLine != "") {
          //look for nested item
-         $openingPos = strpos ($input, "{");
+         $openingPos = strpos($input, "{");
          if ($openingPos !== false) {
             // Found an item inside line, so process it
             // Revert line to input in order to extract the whole item structure
             $input = $cleanLine . "\n" . $input;
             // Look for item closing
-            $nextClosing = findMatchingClosing ($input);
+            $nextClosing = findMatchingClosing($input);
             if ($nextClosing !== false) {
                // extract element from input and process item separatedly
-               $results[] = parseItem (substr($input, 0, $nextClosing+1), $linkingProperty, $parentValue);
+               $results[] = parseItem(substr($input, 0, $nextClosing+1), $linkingProperty, $parentValue);
 
                // Remove the item from input
                $input = trim(substr($input, $nextClosing+1));
             } else {
                // Opening without closing mean bad formatted query, return error
-               returnError ("Malformed query, missing element closing"); //TODO return error location according to graphQL spec
+               returnError("Malformed query, missing element closing"); //TODO return error location according to graphQL spec
             }
 
          } else {
@@ -188,25 +186,26 @@ function parseLevel ($input, $linkingProperty = 0, $parentValue = 0) {
 
 
 // Function to parse one itemtype node
-function parseItem ($input, $linkingProperty = 0, $parentValue = 0, $rootElement = false) {
+function parseItem($input, $linkingProperty = 0, $parentValue = 0, $rootElement = false)
+{
 
    // Remove root query command if passed
-   if($rootElement && (stripos($input,"query") === 0 || stripos($input,"{") === 0)) {
+   if ($rootElement && (stripos($input, "query") === 0 || stripos($input, "{") === 0)) {
       // Get string from first { to last char-1 (skip "query:{" from beginning and last "}" from end )
       // TODO manage optional operation name (for error handling only)
-      $input = trim(substr($input, strpos($input,"{")+1, -1));
+      $input = trim(substr($input, strpos($input, "{")+1, -1));
    }
    // Get field (itemType/related_property) name
-   $field = trim(substr($input, 0, strpos($input,"{")));
+   $field = trim(substr($input, 0, strpos($input, "{")));
 
    //Second get the arguments (filters) for the itemtype with ()
    $filterProperties = array();
    $arguments = array();
-   if (strpos($field,"(") !== false) {
+   if (strpos($field, "(") !== false) {
       // TODO consider other delimiters than "," and beware of " " and """ """
-      $arguments = explode(",",substr($field,strpos($field, "(")+1, -1));
+      $arguments = explode(",", substr($field, strpos($field, "(")+1, -1));
       // Remove arguments from field value
-      $field = trim(substr($field, 0, strpos($input,"(")));
+      $field = trim(substr($field, 0, strpos($input, "(")));
    }
 
    // TODO Get itemtype from field (directly or through related property)****************************************************************
@@ -232,7 +231,8 @@ function parseItem ($input, $linkingProperty = 0, $parentValue = 0, $rootElement
 
 
 // Writes error message to output and terminates processing query
-function returnError ($errorMsg) {
+function returnError($errorMsg)
+{
    // Return data converted to JSON (and compressed if possible)
    echo json_encode(array("errors" => array(array("message" => $errorMsg))));
    // Terminate execution
@@ -241,20 +241,25 @@ function returnError ($errorMsg) {
 
 
 // Gets the position of the closing element matching the first opening element defined
-function findMatchingClosing ($input, $openingChar = "{", $closingChar = "}") {
+function findMatchingClosing($input, $openingChar = "{", $closingChar = "}")
+{
    // TODO beware of " " and """ """ elements
    //Get the position of first opening and return false if not fount
-   $pos = strpos ($input, $openingChar);
-   if ($pos === false) return false;
+   $pos = strpos($input, $openingChar);
+   if ($pos === false) {
+      return false;
+   }
 
    $levelsCount = 1;
 
    do {
       // get next occurence of opening and closing
-      $nextOpen = strpos ($input, $openingChar, $pos + 1);
-      $nextClose = strpos ($input, $closingChar, $pos + 1);
+      $nextOpen = strpos($input, $openingChar, $pos + 1);
+      $nextClose = strpos($input, $closingChar, $pos + 1);
       // If next closing is missing the structure is wrong/incomplete so returning false
-      if ($nextClose === false) return false;
+      if ($nextClose === false) {
+         return false;
+      }
 
       if ($nextOpen !== false || $nextOpen < $nextClose) {
          //If next opening exists and found before next closing store its position and increment levels count
@@ -269,5 +274,3 @@ function findMatchingClosing ($input, $openingChar = "{", $closingChar = "}") {
 
    return $pos;
 }
-
-?>
