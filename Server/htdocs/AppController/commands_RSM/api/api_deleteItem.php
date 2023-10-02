f12bcbe567d6fb686c9f58eddcda1833deb11682
Iván Fernández <ifernandez@redsauce.net>
2023-10-02 22:05:36 +0200

    Update api_deleteItem.php
    Removed unsecure configuration

--- Server/htdocs/AppController/commands_RSM/api/api_deleteItem.php
+++ Server/htdocs/AppController/commands_RSM/api/api_deleteItem.php
@@ -14,8 +14,6 @@ require_once "../utilities/RStools.php";
 require_once "../utilities/RSMitemsManagement.php";
 require_once "./api_headers.php";
 
-$RSallowUncompressed = true;
-
 // definitions
 isset($GLOBALS["RS_POST"]["clientID"  ]) ? $clientID     = $GLOBALS["RS_POST"]["clientID"  ] : dieWithError(400);
 isset($GLOBALS["RS_POST"]["itemTypeID"]) ? $itemTypeID   = $GLOBALS["RS_POST"]["itemTypeID"] : dieWithError(400);
