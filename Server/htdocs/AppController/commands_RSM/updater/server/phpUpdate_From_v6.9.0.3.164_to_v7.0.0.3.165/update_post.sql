a7b839a0ea002cc9df79b6d68a11faa15a15c18c
Alfonso Abascal Fontecha <aabascal@redsauce.net>
2025-11-13 16:00:10 +0100

    Deleted reference to a wrong database name

--- Server/htdocs/AppController/commands_RSM/updater/server/phpUpdate_From_v6.9.0.3.164_to_v7.0.0.3.165/update_post.sql
+++ Server/htdocs/AppController/commands_RSM/updater/server/phpUpdate_From_v6.9.0.3.164_to_v7.0.0.3.165/update_post.sql
@@ -3,10 +3,10 @@ INSERT INTO rs_dbchanges (RS_ID, RS_PREVIOUS_VERSION, RS_NEW_VERSION, RS_EXECUTI
 VALUES (NULL, '6.9.0.3.164', '7.0.0.3.165', NOW(), 'Module scripts Editor has been removed');
 
 # Remove the reference to the scripts editor application
-DELETE FROM rsm1.rs_actions WHERE RS_ID=8;
+DELETE FROM rs_actions WHERE RS_ID=8;
 
 # Also remove the action from all clients
-DELETE FROM rsm1.rs_actions_clients WHERE RS_ACTION_ID=8;
+DELETE FROM rs_actions_clients WHERE RS_ACTION_ID=8;
 
 # Remove the references for removed actions from the rs_actions_group table
 DELETE FROM rs_actions_groups
