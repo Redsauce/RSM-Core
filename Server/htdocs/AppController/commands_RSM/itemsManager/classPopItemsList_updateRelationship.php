db98d893fb8b7731f7f5070bd94654b8a2b4c655
Iván Fernández <ifernandez@redsauce.net>
2025-12-10 17:27:35 +0100

    RSM: Fix for the bug causing the relationships of other customers to be deleted when assigning a new  customer item type to an app item type using the popup menu

--- Server/htdocs/AppController/commands_RSM/itemsManager/classPopItemsList_updateRelationship.php
+++ Server/htdocs/AppController/commands_RSM/itemsManager/classPopItemsList_updateRelationship.php
@@ -19,38 +19,39 @@ require_once "../utilities/RSdatabase.php";
 // Now we build the queries
 
 // Delete old relationships
-$theQuery = RSquery("DELETE FROM rs_item_type_app_relations WHERE RS_ITEMTYPE_APP_ID = '".$GLOBALS['RS_POST']['itemTypeAppID']."' AND RS_CLIENT_ID = '".$GLOBALS['RS_POST']['clientID']."'");
+$theQuery = RSquery("DELETE FROM rs_item_type_app_relations WHERE RS_ITEMTYPE_APP_ID = '".$GLOBALS[$cstRS_POST]['itemTypeAppID']."' AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
 
-$theQuery = RSquery("DELETE FROM rs_item_type_app_relations WHERE RS_ITEMTYPE_ID = '".$GLOBALS['RS_POST']['itemTypeID']."' AND RS_CLIENT_ID = '".$GLOBALS['RS_POST']['clientID']."'");
+$theQuery = RSquery("DELETE FROM rs_item_type_app_relations WHERE RS_ITEMTYPE_ID = '".$GLOBALS[$cstRS_POST][$cstItemTypeID]."' AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
 
-$propertyAppDefRes = RSquery("SELECT RS_ID FROM rs_property_app_definitions WHERE RS_ITEM_TYPE_ID = '".$GLOBALS['RS_POST']['itemTypeAppID']."'");
+$propertyAppDefRes = RSquery("SELECT RS_ID FROM rs_property_app_definitions WHERE RS_ITEM_TYPE_ID = '".$GLOBALS[$cstRS_POST]['itemTypeAppID']."'");
 $propertyAppDefList="";
 while($res=$propertyAppDefRes->fetch_assoc()){
 	$propertyAppDefList.=$res['RS_ID'].",";
 }
 $propertyAppDefList=rtrim($propertyAppDefList,",");
 
-$theQuery = RSquery("DELETE FROM rs_property_app_relations WHERE RS_PROPERTY_APP_ID IN (".$propertyAppDefList.") AND RS_CLIENT_ID = '".$GLOBALS['RS_POST']['clientID']."'");
+$theQuery = RSquery("DELETE FROM rs_property_app_relations WHERE RS_PROPERTY_APP_ID IN (".$propertyAppDefList.") AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
 
-$categoriesRes = RSquery("SELECT RS_CATEGORY_ID FROM rs_categories WHERE RS_ITEMTYPE_ID = '".$GLOBALS['RS_POST']['itemTypeID']."' AND RS_CLIENT_ID = '".$GLOBALS['RS_POST']['clientID']."'");
+$categoriesRes = RSquery("SELECT RS_CATEGORY_ID FROM rs_categories WHERE RS_ITEMTYPE_ID = '".$GLOBALS[$cstRS_POST][$cstItemTypeID]."' AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
 $categoriesList="";
 while($res=$categoriesRes->fetch_assoc()){
 	$categoriesList.=$res['RS_CATEGORY_ID'].",";
 }
 $categoriesList=rtrim($categoriesList,",");
 
-$propertiesRes = RSquery("SELECT RS_PROPERTY_ID FROM rs_item_properties WHERE RS_CATEGORY_ID IN (".$categoriesList.")");
+$propertiesRes = RSquery("SELECT RS_PROPERTY_ID FROM rs_item_properties WHERE RS_CATEGORY_ID IN (".$categoriesList.") AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
+
 $propertiesList="";
 while($res=$propertiesRes->fetch_assoc()){
 	$propertiesList.=$res['RS_PROPERTY_ID'].",";
 }
 $propertiesList=rtrim($propertiesList,",");
 
-$theQuery = RSquery("DELETE FROM rs_property_app_relations WHERE RS_PROPERTY_ID IN (".$propertiesList.") AND RS_CLIENT_ID = '".$GLOBALS['RS_POST']['clientID']."'");
+$theQuery = RSquery("DELETE FROM rs_property_app_relations WHERE RS_PROPERTY_ID IN (".$propertiesList.") AND RS_CLIENT_ID = '".$GLOBALS[$cstRS_POST][$cstClientID]."'");
 
 
 // Insert new relationship
-$theQuery = "INSERT INTO rs_item_type_app_relations (RS_ITEMTYPE_ID, RS_CLIENT_ID, RS_ITEMTYPE_APP_ID, RS_MODIFIED_DATE) VALUES ('".$GLOBALS['RS_POST']['itemTypeID']."', '".$GLOBALS['RS_POST']['clientID']."', '".$GLOBALS['RS_POST']['itemTypeAppID']."', NOW())";
+$theQuery = "INSERT INTO rs_item_type_app_relations (RS_ITEMTYPE_ID, RS_CLIENT_ID, RS_ITEMTYPE_APP_ID, RS_MODIFIED_DATE) VALUES ('".$GLOBALS[$cstRS_POST][$cstItemTypeID]."', '".$GLOBALS[$cstRS_POST][$cstClientID]."', '".$GLOBALS[$cstRS_POST]['itemTypeAppID']."', NOW())";
 
 // Query the database
 $results = RSquery($theQuery);
@@ -61,3 +62,4 @@ $response['result'] = ($results == TRUE) ? "OK" : "NOK";
 // And write XML Response back to the application
 RSReturnArrayResults($response);
 ?>
+
