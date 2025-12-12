1db96fa3222e4c66f26fb7ecfdc0b20f009790ae
Iván Fernández <ifernandez@redsauce.net>
2025-12-12 18:00:07 +0100

    nueva función de generación de tokens, más rápida, sin bug del bucle infinito

--- Server/htdocs/AppController/commands_RSM/api/classLbxTokens_newToken.php
+++ Server/htdocs/AppController/commands_RSM/api/classLbxTokens_newToken.php
@@ -18,23 +18,31 @@ if (empty($GLOBALS[$cstRS_POST][$cstClientID])) {
     dieWithError(400);
 }
 
-// We will use this variable in order to control if the new token exists or not
-$exists = false;
-
 do {
-    // Let's generate a token
-    $token = md5(generateRandomString(256));
+    // We assume the token does not exist
+    $exists = false;
+
+    // Let's generate a token (32 chars hex string)
+    // We use random_bytes for cryptographic security instead of md5(rand)
+    try {
+        $token = bin2hex(random_bytes(16));
+    } catch (Exception $e) {
+        // Fallback if random_bytes fails
+        $token = md5(uniqid(rand(), true));
+    }
 
     // Ask the database for tokens like the new one
     $results = RScountToken($token);
 
     // Obtain the data from the query
-    if ($results) $result = $results->fetch_assoc();
-    
-    // Check if we found a token like ours in the database
-    if ($result['total'] <> 0) $exists = true; // The token is already stored in the database. We must generate a new one
+    if ($results && $result = $results->fetch_assoc()) {
+        // Check if we found a token like ours in the database
+        if ($result['total'] != 0) {
+            $exists = true; // The token is already stored in the database. We must generate a new one
+        }
+    }
 
-} while ($exists == true);
+} while ($exists);
 
 // If the execution reaches this point, the token does not exist so we can insert it
 // RScreateToken() is defined in Server/htdocs/AppController/commands_RSM/utilities/RSMtokensManagement.php
@@ -45,18 +53,3 @@ $response['token'] = $token;
 
 // And write XML Response back to the application
 RSReturnArrayResults($response);
-
-// This function generates a random string of the given length
-function generateRandomString($length = 10) {
-    // This is the list of characters allowed inside the generated string
-    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $randomString = '';
-
-    for ($i = 0; $i < $length; $i++) {
-        // We loop until the desired length is reached, adding random characters
-        $randomString .= $characters[random_int(0, strlen($characters) - 1)];
-    }
-
-    // And return the result
-    return $randomString;
-}
